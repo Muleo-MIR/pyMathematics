1b5d6b1202afe2b3d6d3bc7a1688f4c55821391e
Muleo-MIR <muleo.1g@gmail.com>
2024-09-07 20:55:54 +0100

    ranListGen file directory fixes / quicksort project folder

---
+++ quicksortAlgorithm/ranListGen.py
@@ -0,0 +1,14 @@
+
+#list generator
+
+import numpy as np
+import os
+
+filename = "randomListGen\output.txt"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+f = open(filename, "w") 
+
+lowerBound,upperBound,n = map(int,input('Input lower bound, upper bound and number of elements in list: ').split())
+
+print(list(np.random.randint(low = lowerBound,high = upperBound,size = n)),file = f)
+f.close()
