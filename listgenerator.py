40a61542b7051a13bfd527093e9d384d593a419b
Muleo-MIR <muleo.1g@gmail.com>
2024-09-07 18:41:01 +0100

    listgenerator.py output to file

--- listgenerator.py
+++ listgenerator.py
@@ -2,7 +2,11 @@
 #list generator
 
 import numpy as np
+import sys
+
+file = open("output.txt", "w")
 
 lowerBound,upperBound,n = map(int,input('Input lower bound, upper bound and number of elements in list: ').split())
 
-print(list(np.random.randint(low = lowerBound,high = upperBound,size = n)))
+print(list(np.random.randint(low = lowerBound,high = upperBound,size = n)), file = file)
+file.close()
