7e0ed0ade118f9a409caa22717fc8087a2d3fd06
Muleo-MIR <muleo.1g@gmail.com>
2024-09-07 17:36:14 +0100

    quicksort.py initial upload / fix euclidianAlgorithm.py naming

---
+++ quicksort.py
@@ -0,0 +1,15 @@
+list = [int(item) for item in input('Input list: ').split()]
+
+def quicksort (list):
+    if len (list) <= 1:
+        return list
+    else:
+        pivot = list[0]
+        left = [x for x in list[1:] if x < pivot]
+        right = [x for x in list[1:] if x >= pivot]
+        return quicksort(left) + [pivot] + quicksort(right)
+
+
+sorted_list = quicksort(list)
+print(f'Sorted list:{sorted_list}')
+
